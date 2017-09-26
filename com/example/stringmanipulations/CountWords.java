f2276ae0bc4059686155c3c110615ad889b8aa8f
Rajani Kokkula <rajani.kokkula@gmail.com>
2017-09-26 11:36:29 -0700

    This folder contains programs with String manipulations

--- com/example/stringmanipulations/CountWords.java
+++ com/example/stringmanipulations/CountWords.java
@@ -4,4 +4,15 @@ package com.example.stringmanipulations;
 public class CountWords {
 
 
+    public static void main(String[] args){
+        String str1 = "Betty butter bought some butter";
+        countWordsInString(str1);
+    }
+
+    private static void countWordsInString(String str){
+        String[] myarr = str.split(" ");
+        System.out.println(myarr.length);
+    }
+
+
 }
